ee7ad22fe30af412bcff501e7232c3ffe39767f5
grcndev <gleyce7mit@gmail.com>
2025-05-12 12:36:52 -0300

    modal updated

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -2,6 +2,7 @@ import { OrderItemProps } from "@/providers/order";
 
 export const calculateTotalOrder = (orders: OrderItemProps[]) => {
     return orders.reduce((total, item) => {
+        //@ts-ignore
         const itemTotal = parseFloat(item.product.price) + item.amount;
         return total + itemTotal
     }, 0)
